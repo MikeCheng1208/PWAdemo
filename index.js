fbc4b3b7a10cc03433db11ffc7959b1495d10339
Ｍike <1208966@gmail.com>
2019-03-27 13:48:13 +0800

    init

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-var text = document.getElementById("text");
-var deferredPrompt = null;
+// var text = document.getElementById("text");
+// var deferredPrompt = null;
 
-window.addEventListener('beforeinstallprompt', function (e) {
-    deferredPrompt = e;
-    e.preventDefault();
-    console.log('beforeinstallprompt: ',deferredPrompt);
-});
+// window.addEventListener('beforeinstallprompt', function (e) {
+    // e.preventDefault();
+//     deferredPrompt = e;
+//     console.log('beforeinstallprompt: ',deferredPrompt);
+// });
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./ServiceWorker.js') // 註冊 Service Worker
-    .then(function(reg) {
-        text.innerHTML = "註冊成功";
-        installOk();
-        console.log('註冊成功 Registration succeeded. Scope is ' + reg.scope); // 註冊成功
-    }).catch(function(error) {
-        text.innerHTML = "註冊失敗";
-        console.log('註冊失敗 Registration failed with ' + error); // 註冊失敗
-    });
-}
+// if('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('./ServiceWorker.js') // 註冊 Service Worker
+//     .then(function(reg) {
+//         text.innerHTML = "註冊成功";
+//         installOk();
+//         console.log('註冊成功 Registration succeeded. Scope is ' + reg.scope); // 註冊成功
+//     }).catch(function(error) {
+//         text.innerHTML = "註冊失敗";
+//         console.log('註冊失敗 Registration failed with ' + error); // 註冊失敗
+//     });
+// }
 
 
-function installOk(){
-    var BodyInstall = document.getElementsByTagName("body")[0];
-    BodyInstall.addEventListener('click', function () {
-        console.log('click');
-        if (deferredPrompt != null) {
-            // 异步触发横幅显示
-            deferredPrompt.prompt();
+// function installOk(){
+//     var BodyInstall = document.getElementsByTagName("body")[0];
+//     BodyInstall.addEventListener('click', function () {
+//         console.log('click');
+//         if (deferredPrompt != null) {
+//             // 异步触发横幅显示
+//             deferredPrompt.prompt();
 
-            deferredPrompt.userChoice.then(function (choiceResult) {
-                if (choiceResult.outcome === 'dismissed') {
-                    text.innerHTML = "用户取消安装应用";
-                } else {
-                    text.innerHTML = "用户安装了应用";
-                }
-            });
+//             deferredPrompt.userChoice.then(function (choiceResult) {
+//                 if (choiceResult.outcome === 'dismissed') {
+//                     text.innerHTML = "用户取消安装应用";
+//                 } else {
+//                     text.innerHTML = "用户安装了应用";
+//                 }
+//             });
             
-            deferredPrompt = null;
-        }
-    });
+//             deferredPrompt = null;
+//         }
+//     });
 
 
 
-}
+// }
