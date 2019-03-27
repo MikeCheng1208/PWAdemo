c35cffb862848901326aedffa3b70041ab0c159c
Ｍike <1208966@gmail.com>
2019-03-27 15:34:20 +0800

    init

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('video-store').then(function(cache) {
       return cache.addAll([
-        './index.js',
+        './main.js',
         './index.html',
         './bg.jpeg',
         './index.css',
