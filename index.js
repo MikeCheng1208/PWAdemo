62d239e01d0ae16e79b867f7c56f2341757a09d9
Ｍike <1208966@gmail.com>
2019-03-27 11:03:05 +0800

    init

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./ServiceWorker.js') // 註冊 Service Worker
     .then(function(reg) {
+        alert('註冊成功');
         console.log('註冊成功 Registration succeeded. Scope is ' + reg.scope); // 註冊成功
     }).catch(function(error) {
+        alert('註冊失敗');
         console.log('註冊失敗 Registration failed with ' + error); // 註冊失敗
     });
 }
