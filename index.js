79d60eaef7438bb07c60e304b8e3fd507f622235
Ｍike <1208966@gmail.com>
2019-03-27 11:14:30 +0800

    init

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+var text = document.getElementById("text");
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./ServiceWorker.js') // 註冊 Service Worker
     .then(function(reg) {
-        alert('註冊成功');
+        text.innerHTML = "註冊成功";
+        installOk();
         console.log('註冊成功 Registration succeeded. Scope is ' + reg.scope); // 註冊成功
     }).catch(function(error) {
-        alert('註冊失敗');
+        text.innerHTML = "註冊失敗";
         console.log('註冊失敗 Registration failed with ' + error); // 註冊失敗
     });
+}
+
+function installOk(){
+    var deferredPrompt = null;
+    var BodyInstall = document.getElementsByTagName("body")[0];
+
+    window.addEventListener('beforeinstallprompt', function (e) {
+        deferredPrompt = e;
+    });
+
+    BodyInstall.addEventListener('click', function () {
+        if (deferredPrompt != null) {
+            // 异步触发横幅显示
+            deferredPrompt.prompt();
+
+            deferredPrompt.userChoice.then(function (choiceResult) {
+                if (choiceResult.outcome === 'dismissed') {
+                    text.innerHTML = "用户取消安装应用";
+                } else {
+                    text.innerHTML = "用户安装了应用";
+                }
+            });
+            
+            deferredPrompt = null;
+        }
+    });
+
+
+
 }
